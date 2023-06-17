d1b013101629995bba97845c4407be01f0e145ee
Ben Coleman <benc.uk@gmail.com>
2023-06-17 23:25:42 +0100

    tests!

--- proxy/main_test.go
+++ proxy/main_test.go
@@ -13,6 +13,36 @@ import (
 var nanoProxy = &NanoProxy{}
 var timeout = 5 * time.Second
 
+var simpleConf = config.Config{
+	Rules: []config.Rule{
+		{
+			Path:      "/api",
+			Upstream:  "example",
+			StripPath: true,
+		},
+		{
+			Path:      "/nostrip",
+			Upstream:  "example",
+			StripPath: false,
+		},
+		{
+			Path:     "/badupstream",
+			Upstream: "notexist",
+		},
+	},
+	Upstreams: []config.Upstream{
+		{
+			Name:   "example",
+			Host:   "example.net",
+			Scheme: "https",
+		},
+		{
+			Name: "notexist",
+			Host: "notexist.zzzyyy.dummy",
+		},
+	},
+}
+
 func TestMain(m *testing.M) {
 	os.Setenv("DEBUG", "1")
 
@@ -43,25 +73,8 @@ func TestDebugConfDump200(t *testing.T) {
 	}
 }
 
-func TestProxyPathRule200(t *testing.T) {
-	conf := config.Config{
-		Rules: []config.Rule{
-			{
-				Path:      "/api",
-				Upstream:  "example",
-				StripPath: true,
-			},
-		},
-		Upstreams: []config.Upstream{
-			{
-				Name:   "example",
-				Host:   "example.net",
-				Scheme: "https",
-			},
-		},
-	}
-
-	nanoProxy.processConfig(conf, timeout)
+func TestProxyPath200(t *testing.T) {
+	nanoProxy.processConfig(simpleConf, timeout)
 
 	request, _ := http.NewRequest(http.MethodGet, "/api", nil)
 	response := httptest.NewRecorder()
@@ -70,5 +83,44 @@ func TestProxyPathRule200(t *testing.T) {
 
 	if response.Code != http.StatusOK {
 		t.Errorf("Expected 200, got %d", response.Code)
+	}
+}
+
+func TestProxyPath404(t *testing.T) {
+	nanoProxy.processConfig(simpleConf, timeout)
+
+	request, _ := http.NewRequest(http.MethodGet, "/cake", nil)
+	response := httptest.NewRecorder()
+
+	nanoProxy.mainHandler(response, request)
+
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected 404, got %d", response.Code)
+	}
+}
+
+func TestProxyPathNoStrip404(t *testing.T) {
+	nanoProxy.processConfig(simpleConf, timeout)
+
+	request, _ := http.NewRequest(http.MethodGet, "/nostrip", nil)
+	response := httptest.NewRecorder()
+
+	nanoProxy.mainHandler(response, request)
+
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected 404, got %d", response.Code)
+	}
+}
+
+func TestProxyPathBadUpstream502(t *testing.T) {
+	nanoProxy.processConfig(simpleConf, timeout)
+
+	request, _ := http.NewRequest(http.MethodGet, "/badupstream", nil)
+	response := httptest.NewRecorder()
+
+	nanoProxy.mainHandler(response, request)
+
+	if response.Code != http.StatusBadGateway {
+		t.Errorf("Expected 502, got %d", response.Code)
 	}
 }
